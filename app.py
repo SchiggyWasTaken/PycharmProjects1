a7276d295b2163bfa6d228538b2a004aa2218ec8
karstenschygulla <karstenschygulla@gmail.com>
2021-10-06 17:56:44 +0200

    spielerei . vllt kaputtmacherei TEIL 2

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Word!</p>"
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(debug = True)
 
